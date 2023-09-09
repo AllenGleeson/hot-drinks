0ca8d5c555535f7c84bc2778c91b03f32c910f37
AllenGleeson <allen_gleeson@hotmail.com>
2023-09-09 16:40:09 +0100

    Adjusting jumbotron.

--- src/features/home/jumbotron.js
+++ src/features/home/jumbotron.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import temp from '../../assests/temp.webp'
 
 const Jumbotron = () => {
     return (
-        <section id='jumbotron' class="mb-3 align-content-center d-flex justify-content-center align-items-center">
-            <div className='overlay'>
-                <h1>Welcome to Hot Drinks. Swing by some time for a delicious brew.</h1>
+        <section class="jumbotron">
+            <img src={temp} alt="cafe" />
+            <div class="caption">
+                <h2>Welcome To Hot Drinks</h2>
+                <p>Swing by sometime to have a delicious brew.</p>
             </div>
         </section>
     );
